d1b337989284d10c63dffc6225eb7236a5e3ea82
Vera D <veradanicode@gmail.com>
2025-09-26 18:40:52 +0100

    first commit:initialization and setup1

---
+++ frontend/storyblok/src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+
+import Page from "./components/Page";
+import Teaser from "./components/Teaser";
+import Grid from "./components/Grid";
+import Feature from "./components/Feature";
+
+storyblokInit({
+  accessToken: "5rQxhkYeai48s7U5TrltGQtt",
+  use: [apiPlugin],
+  components: {
+    page: Page,
+    teaser: Teaser,
+    grid: Grid,
+    feature: Feature,
+  },
+  apiOptions: {
+    // for spaces located in the US or China:
+    // region: "us" or "cn", // you need to specify the region
+    region: ''
+  }
+});
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
